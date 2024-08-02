f1d39d437e8393e600a973882c74c3887bbf75b4
devsrijit <srijitghosh7331@gmail.com>
2024-08-02 21:32:12 +0530

    feat: Add lazy loading for images in DashboardPage
    This commit adds lazy loading for images in the DashboardPage component. The useScroll and useTransform hooks from the framer-motion library are used to track the scroll progress and dynamically adjust the path lengths of the GoogleGeminiEffect component. This enhancement improves the page load performance by loading images only when they are visible on the screen.

--- src/app/(main)/(pages)/dashboard/page.tsx
+++ src/app/(main)/(pages)/dashboard/page.tsx
@@ -1,13 +1,35 @@
+"use client";
+import { useScroll, useTransform } from "framer-motion";
 import React from "react";
+import { GoogleGeminiEffect } from "@/components/global/google-gemini-effect";
 
-const DashboardPage = () => {
+export default function DashboardPage() {
+  const ref = React.useRef(null);
+  const { scrollYProgress } = useScroll({
+    target: ref,
+    offset: ["start start", "end start"],
+  });
+
+  const pathLengthFirst = useTransform(scrollYProgress, [0, 0.8], [0.2, 1.2]);
+  const pathLengthSecond = useTransform(scrollYProgress, [0, 0.8], [0.15, 1.2]);
+  const pathLengthThird = useTransform(scrollYProgress, [0, 0.8], [0.1, 1.2]);
+  const pathLengthFourth = useTransform(scrollYProgress, [0, 0.8], [0.05, 1.2]);
+  const pathLengthFifth = useTransform(scrollYProgress, [0, 0.8], [0, 1.2]);
+
   return (
-    <div className="flex flex-col gap-4 relative">
-      <h1 className="text-4xl sticky top-0 z-[10] p-6 bg-background/50 backdrop-blur-lg flex items-center border-b">
-        Dashboard
-      </h1>
+    <div
+      className="bg-black w-full dark:border dark:border-white/[0.1] rounded-md relative pt-40 overflow-clip h-full"
+      ref={ref}
+    >
+      <GoogleGeminiEffect
+        pathLengths={[
+          pathLengthFirst,
+          pathLengthSecond,
+          pathLengthThird,
+          pathLengthFourth,
+          pathLengthFifth,
+        ]}
+      />
     </div>
   );
-};
-
-export default DashboardPage;
+}
