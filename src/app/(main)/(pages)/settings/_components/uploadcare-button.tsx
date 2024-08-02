26f5d8495e833c09ca2b114b7a5a081f7d61c495
devsrijit <srijitghosh7331@gmail.com>
2024-08-02 20:42:50 +0530

    chore: Update package.json to include "prisma generate" in postinstall script
    The postinstall script in the package.json file was updated to include the command "prisma generate" after the "next lint" command. This change ensures that the Prisma client is generated automatically during the installation process.

--- src/app/(main)/(pages)/settings/_components/uploadcare-button.tsx
+++ src/app/(main)/(pages)/settings/_components/uploadcare-button.tsx
@@ -1,48 +1,36 @@
-'use client'
-import React, { useEffect, useRef } from 'react'
-import * as LR from '@uploadcare/blocks'
-import { useRouter } from 'next/navigation'
+"use client";
+import React, { useEffect, useRef } from "react";
+import { FileUploaderRegular } from "@uploadcare/react-uploader";
+import "@uploadcare/react-uploader/core.css";
+import { useRouter } from "next/navigation";
 
 type Props = {
-  onUpload: (e: string) => any
-}
-
-LR.registerBlocks(LR)
+  onUpload: (e: string) => any;
+};
 
 const UploadCareButton = ({ onUpload }: Props) => {
-  const router = useRouter()
-  const ctxProviderRef:any = useRef<
-    typeof LR.UploadCtxProvider.prototype & LR.UploadCtxProvider
-  >(null)
+  const router = useRouter();
 
-  useEffect(() => {
-    const handleUpload = async (e: any) => {
-      const file = await onUpload(e.detail.cdnUrl)
-      if (file) {
-        router.refresh()
-      }
+  const handleUploadSuccess = async (e: any) => {
+    const file = await onUpload(e.fileInfo.cdnUrl);
+    if (file) {
+      router.refresh();
     }
-    ctxProviderRef.current.addEventListener('file-upload-success', handleUpload)
-  }, [])
+  };
 
   return (
     <div>
-      <lr-config
-        ctx-name="my-uploader"
-        pubkey="a9428ff5ff90ae7a64eb"
-      />
-
-      <lr-file-uploader-regular
-        ctx-name="my-uploader"
-        css-src={`https://cdn.jsdelivr.net/npm/@uploadcare/blocks@0.35.2/web/lr-file-uploader-regular.min.css`}
-      />
-
-      <lr-upload-ctx-provider
-        ctx-name="my-uploader"
-        ref={ctxProviderRef}
+      <FileUploaderRegular
+        pubkey="888328bd9961325a729a"
+        maxLocalFileSizeBytes={500000000}
+        multiple={false}
+        imgOnly={true}
+        sourceList="local, url, camera, dropbox, facebook, gdrive, gphotos, instagram"
+        classNameUploader="my-config"
+        onFileUploadSuccess={handleUploadSuccess}
       />
     </div>
-  )
-}
+  );
+};
 
-export default UploadCareButton
+export default UploadCareButton;
