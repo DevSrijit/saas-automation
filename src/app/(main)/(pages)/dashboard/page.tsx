828ad5fddd44be34c8aa90e3c93fc165740d5a21
devsrijit <srijitghosh7331@gmail.com>
2024-08-02 21:54:24 +0530

    refactor: Update DashboardPage component with lazy loading for images

--- src/app/(main)/(pages)/dashboard/page.tsx
+++ src/app/(main)/(pages)/dashboard/page.tsx
@@ -1,35 +1,30 @@
 "use client";
-import { useScroll, useTransform } from "framer-motion";
 import React from "react";
-import { GoogleGeminiEffect } from "@/components/global/google-gemini-effect";
+import { BackgroundBeams } from "@/components/global/background-beams";
+import { Button } from "@/components/ui/button";
+import { useRouter } from 'next/navigation';
 
 export default function DashboardPage() {
-  const ref = React.useRef(null);
-  const { scrollYProgress } = useScroll({
-    target: ref,
-    offset: ["start start", "end start"],
-  });
-
-  const pathLengthFirst = useTransform(scrollYProgress, [0, 0.8], [0.2, 1.2]);
-  const pathLengthSecond = useTransform(scrollYProgress, [0, 0.8], [0.15, 1.2]);
-  const pathLengthThird = useTransform(scrollYProgress, [0, 0.8], [0.1, 1.2]);
-  const pathLengthFourth = useTransform(scrollYProgress, [0, 0.8], [0.05, 1.2]);
-  const pathLengthFifth = useTransform(scrollYProgress, [0, 0.8], [0, 1.2]);
+  const router = useRouter();
 
   return (
-    <div
-      className="bg-black w-full dark:border dark:border-white/[0.1] rounded-md relative pt-40 overflow-clip h-full"
-      ref={ref}
-    >
-      <GoogleGeminiEffect
-        pathLengths={[
-          pathLengthFirst,
-          pathLengthSecond,
-          pathLengthThird,
-          pathLengthFourth,
-          pathLengthFifth,
-        ]}
-      />
+    <div className="h-[40rem] w-full rounded-md bg-neutral-950 relative flex flex-col items-center justify-center antialiased">
+      <div className="max-w-5xl mx-auto p-4">
+        <h1 className="relative z-10 text-xl md:text-7xl bg-clip-text text-transparent bg-gradient-to-b from-neutral-200 to-neutral-600 text-center font-sans font-bold">
+          Create your automations with ease
+        </h1>
+        <p className="text-neutral-500 max-w-xl mx-auto my-2 text-lg text-center relative z-10">
+          Head over to Workflows to create your first automation. You can use
+          notion, discord, slack and many more apps to create your automations.
+          Experience
+        </p>
+        <div className="flex justify-center items-center">
+          <Button className="mt-4 z-50" onClick={() => router.push('/workflows')}>
+            Workflows
+          </Button>
+        </div>
+      </div>
+      <BackgroundBeams />
     </div>
   );
 }
