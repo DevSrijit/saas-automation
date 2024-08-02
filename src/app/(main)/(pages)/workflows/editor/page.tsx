e8fce2764f9b90b904202f8f26333b6207e57a26
devsrijit <srijitghosh7331@gmail.com>
2024-08-03 00:21:53 +0530

    feat: Redirect user to workflows page if accessing invalid route
    This commit updates the `Page` component in `src/app/(main)/(pages)/workflows/editor/page.tsx` to redirect the user to the workflows page if they try to access an invalid route. This improves the user experience by ensuring that they are directed to a valid page instead of seeing a 404 error.

--- src/app/(main)/(pages)/workflows/editor/page.tsx
+++ src/app/(main)/(pages)/workflows/editor/page.tsx
@@ -1,10 +1,13 @@
-import React from 'react'
+"use client"
+import {useRouter} from 'next/navigation'
 
 type Props = {}
 
 const Page = (props: Props) => {
-  //CHALLENGE: If the user tries to access this route you should send them to their first workflow they have or create one or you can have your own behavior.
-  return <div>404</div>
+    const router = useRouter()
+    return (
+      router.push('/workflows')
+    )
 }
 
 export default Page
